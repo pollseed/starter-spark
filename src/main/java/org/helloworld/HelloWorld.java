d1172ae311adb0f6f60f618615d9c0670ba64128
pollseed <cb.so.devope+0001@gmail.com>
2015-12-21 18:38:53 +0900

    add test get,post

--- src/main/java/org/helloworld/HelloWorld.java
+++ src/main/java/org/helloworld/HelloWorld.java
@@ -1,7 +1,22 @@
 package org.helloworld;
 
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import com.google.gson.Gson;
+
 public class HelloWorld {
+    private static final Gson G = new Gson();
+
     public static void main(String[] args) {
-        spark.Spark.get("/hello", (request, response) -> "Hello, World!");
+
+        get("/hello/:name", (request, response) -> "Hello, World! " + request.params("name"));
+        post("/hello", (request, response) -> "Hello, World" + request.body());
+
+        get("/ip", (request, response) -> G.toJson(request.ip()));
+        get("/host", (request, response) -> G.toJson(request.host()));
+        get("/user_agent", (request, response) -> G.toJson(request.userAgent()));
+
+        get("/q", (request, response) -> G.toJson(request.queryString()));
     }
 }
